810ad24d2372c39dd562efe599939f0f2b39806f
Kim Wysocki <kcwysocka@icloud.com>
2020-08-21 15:31:30 -0400

    add fizzbuzz solution in Golang (prints to console)

--- Golang/fizzbuzz.go
+++ Golang/fizzbuzz.go
@@ -3,11 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fizzBuzz(5)
+	fmt.Println("Please enter a number: ")
+
+	var num int
+	fmt.Scanln(&num)
+
+	fizzBuzz(num)
 }
 
-func fizzBuzz(n int) string {
-	const x string = "hello world"
-	fmt.Println(x)
-	return x
+func fizzBuzz(n int) {
+
+	for i := 1; i <= n; i++ {
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
