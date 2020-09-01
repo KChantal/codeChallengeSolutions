213a5ada28260493c2c65bb94a12dc576d761779
Kim Wysocki <kcwysocka@icloud.com>
2020-09-01 12:34:26 -0400

    comment out main() in Go files

--- Golang/fibonacci.go
+++ Golang/fibonacci.go
@@ -1,16 +1,14 @@
 package main
 
-import "fmt"
+// func main() {
+// 	fmt.Println("Please enter a number: ")
 
-func main() {
-	fmt.Println("Please enter a number: ")
+// 	var num int
+// 	fmt.Scanln(&num)
 
-	var num int
-	fmt.Scanln(&num)
-
-	result := fibonacci(num)
-	fmt.Println(result)
-}
+// 	result := fibonacci(num)
+// 	fmt.Println(result)
+// }
 
 func fibonacci(n int) int {
 	if n <= 1 {
